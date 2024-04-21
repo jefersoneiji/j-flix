0e80adb29186b19bedb9ab28d60452a478651ad0
jeferson eiji <jeferson.eiji@icloud.com>
2024-04-21 17:00:36 -0300

    test (setup)!: created basic example test

---
+++ src/components/first.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event'
+
+import { First } from "./first";
+
+test('should render counter equals 0', () => {
+    render(<First />)
+
+    expect(screen.getByText(/count is 0/i)).toBeTruthy()
+})
+
+test('should increase counter to 1', async () => {
+    render(<First />)
+
+    await userEvent.click(screen.getByRole('button', { name: /increase/ }))
+
+    expect(screen.getByText(/count is 1/i)).toBeTruthy()
+})
