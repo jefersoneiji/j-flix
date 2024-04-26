68b866869c2162b442a89566bd40b664c9164323
jeferson eiji <jeferson.eiji@icloud.com>
2024-04-26 09:57:10 -0300

    feat (router)!: added redirect in case of non matching routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 
 import { App } from "./App";
 import { Home } from "./modules/home/home";
@@ -25,5 +25,6 @@ export const router = createBrowserRouter([
             }
         ]
     },
+    { path: "*", element: <Navigate to="/" replace /> }
 
 ])
